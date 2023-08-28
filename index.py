439953604682f9db83b5b6f0d415f0e998a0724f
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-08-28 23:10:34 +0530

    Update index.py

--- index.py
+++ index.py
@@ -15,12 +15,13 @@ bot = Bot(TOKEN)
 
 def welcome(update, context) -> None:
     update.message.reply_text(f"Hello {update.message.from_user.first_name}, 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 𝙳𝙰𝚇𝚇 𝙼𝚄𝚂𝙸𝙲 𝙱𝙾𝚃.\n"
-                              f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
-    update.message.reply_text("👇 Enter Movie Name 👇")
+                              f"🔥🔥 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗬𝗼𝘂𝗿 𝗙𝗮𝘃𝗼𝘂𝗿𝗶𝘁𝗲 𝗠𝗼𝘃𝗶𝗲𝘀 𝗙𝗼𝗿 💯 𝗙𝗿𝗲𝗲 𝗔𝗻𝗱 🍿 𝗘𝗻𝗷𝗼𝘆 𝗶𝘁. 𓆠")
+    update.message.reply_text("👇 𝐄𝐍𝐓𝐄𝐑 𝐘𝐎𝐔𝐑 𝐌𝐎𝐕𝐈𝐄 𝐍𝐀𝐌𝐄 𝐁𝐀𝐁𝐘 🫰🏼
+    👇")
 
 
 def find_movie(update, context):
-    search_results = update.message.reply_text("Processing...")
+    search_results = update.message.reply_text("🔍\n𝗣𝗿𝗼𝗰𝗲𝘀𝘀𝗶𝗻𝗴...........")
     query = update.message.text
     movies_list = search_movies(query)
     if movies_list:
