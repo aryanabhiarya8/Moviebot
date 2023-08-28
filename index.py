a256c9c073fa9d07757fe18f2b70a05eaef6cd9a
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-08-28 22:42:40 +0530

    Update index.py

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ bot = Bot(TOKEN)
 
 
 def welcome(update, context) -> None:
-    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
+    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to DAXX Movies.\n"
                               f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
     update.message.reply_text("👇 Enter Movie Name 👇")
 
